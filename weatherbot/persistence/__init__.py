9b1d67d711a6c4c11633fe4e22902c3ad7fe1403
Michael Broner <crab2007@gmail.com>
2026-08-06 08:59:01 +0300

    feat: expose persistence API

---
+++ weatherbot/persistence/__init__.py
@@ -0,0 +1,53 @@
+"""Durable, atomic, backend-neutral event storage and startup recovery."""
+
+from weatherbot.persistence.errors import (
+    ConcurrentDecisionError,
+    CorruptLedgerError,
+    DuplicateIntentError,
+    MigrationError,
+    PersistenceError,
+    RecoveryRequiredError,
+    SchemaVersionError,
+    StoreClosedError,
+)
+from weatherbot.persistence.migrations import CURRENT_SCHEMA_VERSION
+from weatherbot.persistence.recovery import (
+    AdapterMetadata,
+    DecisionClaim,
+    PendingDecisionRecovery,
+    PendingOrderRecovery,
+    RecoveryAction,
+    StartupRecovery,
+)
+from weatherbot.persistence.store import (
+    AppendResult,
+    DecisionClaimResult,
+    SQLiteEventStore,
+    StateCheckpoint,
+    initialize_database,
+    restore_backup,
+)
+
+__all__ = [
+    "AdapterMetadata",
+    "AppendResult",
+    "CURRENT_SCHEMA_VERSION",
+    "ConcurrentDecisionError",
+    "CorruptLedgerError",
+    "DecisionClaim",
+    "DecisionClaimResult",
+    "DuplicateIntentError",
+    "MigrationError",
+    "PendingDecisionRecovery",
+    "PendingOrderRecovery",
+    "PersistenceError",
+    "RecoveryAction",
+    "RecoveryRequiredError",
+    "SQLiteEventStore",
+    "SchemaVersionError",
+    "StartupRecovery",
+    "StateCheckpoint",
+    "StoreClosedError",
+    "initialize_database",
+    "restore_backup",
+]
