5b468ed059f613f2c075dbce3f37c07851bf8bff
Michael Broner <crab2007@gmail.com>
2026-08-12 13:51:57 +0300

    Export calibrated forecasting contracts

--- weatherbot/forecasting/__init__.py
+++ weatherbot/forecasting/__init__.py
@@ -1,5 +1,22 @@
-"""Forecast and observation provenance for signal generation."""
+"""Forecast, observation, and calibrated probability contracts."""
 
+from weatherbot.forecasting.calibration import (
+    CalibrationArtifact,
+    CalibrationDiagnostics,
+    CalibrationError,
+    CalibrationGroup,
+    CalibrationGroupKey,
+    CalibrationSample,
+    CalibratedTemperatureModel,
+    DistributionKind,
+    EmpiricalResidualDistribution,
+    GroupLevel,
+    NormalResidualDistribution,
+    ProbabilityEstimate,
+    Season,
+    calibration_artifact_from_json,
+    load_calibration_artifact,
+)
 from weatherbot.forecasting.model import (
     DailyHighForecast,
     ForecastSource,
@@ -14,12 +31,27 @@ from weatherbot.forecasting.providers import (
 )
 
 __all__ = [
+    "CalibrationArtifact",
+    "CalibrationDiagnostics",
+    "CalibrationError",
+    "CalibrationGroup",
+    "CalibrationGroupKey",
+    "CalibrationSample",
+    "CalibratedTemperatureModel",
     "DailyHighForecast",
+    "DistributionKind",
+    "EmpiricalResidualDistribution",
     "ForecastSource",
+    "GroupLevel",
+    "NormalResidualDistribution",
     "ObservationSource",
+    "ProbabilityEstimate",
+    "Season",
     "TemperatureObservation",
     "WeatherInputError",
     "WeatherInputSnapshot",
+    "calibration_artifact_from_json",
+    "load_calibration_artifact",
     "parse_aviation_weather_metar",
     "parse_open_meteo_daily_highs",
 ]
